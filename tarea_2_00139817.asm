97d96ad0746f83f730de7e652342a07642b9cea5
Bryanceren <00139817@uca.edu.sv>
2020-04-26 15:06:30 -0600

    update del ejercicio 1

---
+++ tarea_2_00139817.asm
@@ -0,0 +1,39 @@
+
+;;;;;;;;;;;;;;;;EJERCICIO 1 ;;;;;;;;;;;;;;;;
+
+    org    100h
+    
+
+;carnet 00139817
+section	.text
+	mov	cx, 0000h
+    mov bx, 0000h
+
+	mov	ax, 3d
+
+	add	ax, 9d
+
+	add	ax, 8d
+
+	add	ax, 1d
+
+	add	ax, 7d
+
+	mov	bx, 5d
+
+	div	bx
+
+	mov	di, 0d
+	mov	cx, [len]
+
+write	mov	bl, [comnt+di]
+	mov	[di+200h], bl
+	inc	di
+
+	loop	write
+
+section.data
+    comnt      db      "En el segundo"
+    len         equ     $-comnt
+
+
