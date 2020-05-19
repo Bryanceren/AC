e9776ecb30e83f491f19ab3fc7c3dd8dcf97c83e
Bryanceren <00139817@uca.edu.sv>
2020-05-19 01:21:58 -0600

    tarea 3 inicio

---
+++ tarea_3_00139817.asm
@@ -0,0 +1,97 @@
+org 	100h
+
+section .text
+
+	call 	texto	
+	call 	cursor
+	call 	phrase
+    call    phrase1
+    call    phrase2
+	call	kbwait
+
+	int 	20h
+
+texto:	mov 	ah, 00h
+	mov	al, 03h
+	int 	10h
+	ret
+
+cursor: mov	ah, 01h
+	mov 	ch, 00000000b
+	mov 	cl, 00001110b
+		;   IRGB
+	int 	10h
+	ret
+
+w_char:	mov 	ah, 09h
+	mov 	al, cl
+	mov 	bh, 0h
+	mov 	bl, 00001111b
+	mov 	cx, 1h
+	int 	10h
+	ret
+
+kbwait: mov 	ax, 0000h
+	int 	16h
+	ret
+
+m_cursr:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	mov 	dh, 6d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+m_cursr1:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	mov 	dh, 12d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+m_cursr2:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	mov 	dh, 18d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+
+phrase:	mov 	di, 29d
+lupi:	mov 	cl, [msg+di-29d]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len
+	jb	lupi
+	ret
+
+phrase1:	mov 	di, 19d
+lupi1:	mov 	cl, [msg1+di-19d]
+	call    m_cursr1
+	call 	w_char
+	inc	di
+	cmp 	di, len1
+	jb	lupi1
+	ret
+
+phrase2:	mov 	di, 35d
+lupi2:	mov 	cl, [msg2+di-35d]
+	call    m_cursr2
+	call 	w_char
+	inc	di
+	cmp 	di, len2
+	jb	lupi2
+	ret
+
+
+
+section .data
+msg	db 	"hey mira ve la onda esta asi ve"
+len 	equ	$-msg+29d
+
+msg1	db 	"que pucha me gustas mi amor"
+len1 	equ	$-msg1+19d
+
+msg2	db 	"que te quede claro"
+len2	equ	$-msg2+35d
